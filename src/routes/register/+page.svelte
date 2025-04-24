2e113b75bd60faa2b9034c3bcacf74942cc9f7eb
Eric <emoolenbeek@gmail.com>
2025-04-23 21:34:17 -0400

    create register form

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
+</script>
+
+<h1>Register</h1>
+<form method="post" action="?/register" use:enhance>
+	<label>
+		Username
+		<input name="username" />
+	</label>
+	<label>
+		Password
+		<input type="password" name="password" />
+	</label>
+	<button>Register</button>
+</form>
+<p style="color: red">{form?.message ?? ''}</p>
